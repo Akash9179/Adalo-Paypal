749f8f10691a0f9077d48b30bc16b6c65278e165
Rajul Verma <97723088+XtremyS@users.noreply.github.com>
2023-01-30 11:00:25 +0530

    adalo paypal component

---
+++ src/components/PaypalGateway/index.js
@@ -0,0 +1,77 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
+const PaypalGateway = (props) => {
+  console.log(props);
+
+  const amount = `${props.amount}`;
+  const currency = `${props.currency}`;
+  const style = { layout: "vertical" };
+
+  return (
+    <>
+      <View>
+        {props.client_id ? (
+          ""
+        ) : (
+          <h4 style={{ color: "red", textAlign: "center" }}>
+            **Please Enter Client ID and Refresh**
+          </h4>
+        )}
+
+        <PayPalScriptProvider
+          options={{
+            "client-id": `${props.client_id}`,
+            components: "buttons",
+            currency: `${props.currency}`,
+          }}
+        >
+          <PayPalButtons
+            style={style}
+            disabled={false}
+            forceReRender={[amount, currency, style]}
+            fundingSource={undefined}
+            createOrder={(data, actions) => {
+              return actions.order
+                .create({
+                  purchase_units: [
+                    {
+                      amount: {
+                        currency_code: currency,
+                        value: amount,
+                      },
+                    },
+                  ],
+                })
+                .then((orderId) => {
+                  return orderId;
+                })
+                .catch((err) => {
+                  console.log(err);
+                });
+            }}
+            onApprove={async function (data, actions) {
+              return actions.order.capture().then(function (details) {
+                if (details.status === "COMPLETED") {
+                  props.actions.useActions();
+                  console.log(details, "DETAILS");
+                }
+              });
+            }}
+          />
+        </PayPalScriptProvider>
+      </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
+export default PaypalGateway;
